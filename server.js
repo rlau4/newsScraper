c34db1978cc470e3595cb6b275d3a9f7400b3adf
rlau4 <p.lau.004@gmail.com>
2019-07-15 11:37:29 -0400

    working scrape

--- server.js
+++ server.js
@@ -12,67 +12,69 @@ var app = express();
 
 mongoose.connect("mongodb://localhost/newsScraper", { useNewUrlParser: true });
 
-app.get("/scrape", function(req, res){
-    axios.get("https://www.nytimes.com/section/sports").then(function(response){
+app.get("/scrape", function (req, res) {
+    axios.get("https://www.nytimes.com/section/sports").then(function (response) {
         var $ = cheerio.load(response.data);
 
-        $("li article").each(function(i, element){
-            var result = {};
+        var results = [];
 
-            result.title = $(this)
-                .children(".css-10wtrdb")
-                .text();
-            result.link = $(this)
-                .children(".css-10wtrdb")
-                .attr("href");
+        $("#collection-sports article.css-15cbhtu").each(function (i, element) {
 
-            db.Article.create(result)
-                .then(function(dbArticle) {
-                    console.log(dbArticle);
-                }) 
-                .catch(function(err){
-                    console.log(err); 
-                });
+            var title = $(element).children("div.css-10wtrbd").children("h2.e134j7ei0").find("a").text();
+
+            var link = $(element).children("div.css-10wtrbd").children("h2.e134j7ei0").find("a").attr("href");
+
+            var summary = $(element).children("div.css-10wtrbd").children("p.e134j7ei1").text();
+
+            var image = $(element).children("figure.photo").children("a").children("img").attr("src");
+
+            results.push({
+                title: title,
+                link: link,
+                summary: summary,
+                image: image
+            });
         });
-        res.send("Scrape Completed");
+        console.log(results);
     });
+    res.send("Scrape Completed");
 });
 
-app.get("/articles", function(err, res){
+app.get("/articles", function (err, res) {
 
     db.Article.find({})
-        .then(function(dbArticle){
+        .then(function (dbArticle) {
             res.json(dbArticle);
         })
-        .catch(function(err){
+        .catch(function (err) {
             res.json(err);
         });
 });
 
-app.get("/article/:id", function(req, res){
-    db.Article.findOne({_id: req.params._id})
+app.get("/article/:id", function (req, res) {
+    db.Article.findOne({ _id: req.params._id })
         .populate("note")
-        .then(function(dbArticle){
+        .then(function (dbArticle) {
             res.json(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             res.json(err);
         });
 });
 
-app.post("/articles/:id", function(req, res){
+app.post("/articles/:id", function (req, res) {
     db.Note.create(req.body)
-        .then(function(dbNote){
-            return db.Article.findOneAndUpdate({ _id: req.params.id}, {note: dbNote._id}, {new: true});
+        .then(function (dbNote) {
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
         })
-        .then(function(dbArticle){
+        .then(function (dbArticle) {
             res.json(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
             res.json(err);
         });
 });
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log("App Listening on port " + PORT)
 });
